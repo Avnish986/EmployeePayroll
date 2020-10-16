a907d92a59f897c9c410c9d5d8cbbf99223602df
avnish986 <avnishgupta986@gmail.com>
2020-10-16 16:24:37 +0530

    [Avnish]Added:adding reading of a file

--- src/main/java/com/employee/details/EmployeePayrollData.java
+++ src/main/java/com/employee/details/EmployeePayrollData.java
@@ -25,6 +25,6 @@ public class EmployeePayrollData {
 
 	@Override
 	public String toString() {
-		return ("Id is: " + getId() + " Name is: " + getName() + " Salary is: " + getSalary());
+		return "Id= " + getId() + ", Name= " + getName() + ", Salary= " + getSalary();
 	}
 }
